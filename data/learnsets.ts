36651442eaf5f3acd07de50a6a13fa0a7853f88b
Orange <61860555+ChaoticProductions@users.noreply.github.com>
2023-09-08 19:00:57 -0700

    Add files via upload

--- data/learnsets.ts
+++ data/learnsets.ts
@@ -50008,6 +50008,12 @@ export const Learnsets: {[speciesid: string]: LearnsetData} = {
 			{generation: 7, level: 10, moves: ["uproar", "confide", "disarmingvoice"], pokeball: "cherishball"},
 		],
 	},
+	rotom: {
+		learnset: {
+			mysticalfire: ["9L1"],
+			willowisp: ["9L1"],
+			infernalparade: ["9L1"],
+	},
 	rotomheat: {
 		learnset: {
 			overheat: ["9L1", "8L1", "7R", "6R", "5R", "4R"],
